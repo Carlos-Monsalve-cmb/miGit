f4c62c882ee0fcb525d9e7dcca81a6011e1187d0
CarlosMonsalve <cmb@siniestrauto.com>
2021-04-27 16:33:29 +0200

    Añadiendo archivo .php

---
+++ Cosas/images.php
@@ -0,0 +1,26 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Image Preset Sizes
+|--------------------------------------------------------------------------
+|
+| Specify the preset sizes you want to use in your code. Only these preset
+| will be accepted by the controller for security.
+|
+| Each preset exists of a width and height. If one of the dimensions are
+| equal to 0, it will automatically calculate a matching width or height
+| to maintain the original ratio.
+|
+| If both dimensions are specified it will automatically crop the
+| resulting image so that it fits those dimensions.
+|
+*/
+
+$config['image_sizes']['small'] = array(75, 50);
+$config['image_sizes']['medium'] = array(1024, 683);
+$config['image_sizes']['large'] = array(1024, 0);
+$config['image_sizes']['featured'] = array(219, 123);
+$config['image_sizes']['list'] = array(480, 320);
+
+/* End of file images.php */
+/* Location: ./app/config/images.php */
